737795d3d0bd6b8cba64adaf0651f70e2d53167d
Adam Jacquier-Parr <aljparr0@gmail.com>
2020-10-03 18:00:37 +1000

    WIP

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -14,33 +14,35 @@ const ContactForm = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
   const [loading, setLoading] = useState(false)
   const [sent, setSent] = useState(false)
+  const [sendError, setSendError] = useState(false)
 
   const handleOnChange = (field, value) => {
     dispatch({ field, value })
   }
 
   const handleOnClick = async event => {
-    const API_HOST = "http://localhost:5000"
+    const API_HOST = "http://localhost:5000";
 
-    if (loading || sent) return
+    if (loading || sent) return;
 
     setLoading(true)
-    // let res = await axios.post(`${API_HOST}/contact`, {
-    //   name,
-    //   email,
-    //   subject,
-    //   description,
-    // })
-    let res = await axios({
-      method: "POST",
-      url: `${API_HOST}/contact`,
-      data: {
-        name,
-        email,
-        subject,
-        description,
-      },
-    });
+    let res;
+    try {
+      res = await axios({
+        method: "POST",
+        url: `${API_HOST}/contact`,
+        data: {
+          name,
+          email,
+          subject,
+          description,
+        },
+      });
+    } catch (err) {
+      setSendError(err.response.data)
+      setLoading(false)
+      return
+    }
     setLoading(false)
 
     switch (res.status) {
@@ -55,33 +57,44 @@ const ContactForm = () => {
   const { name, email, subject, description } = state
   return (
     <div className="u-flex u-flexCol ContactForm">
-      {sent && <div>Message sent!</div>}
-      <h5>
-        Let us know how we can help <i>you</i>
-      </h5>
-      <TextInput
-        label="Name"
-        value={name}
-        onChange={event => handleOnChange("name", event.target.value)}
-      />
-      <TextInput
-        label="Email"
-        value={email}
-        onChange={event => handleOnChange("email", event.target.value)}
-      />
-      <TextInput
-        label="Subject"
-        value={subject}
-        onChange={event => handleOnChange("subject", event.target.value)}
-      />
-      <Textarea
-        label="Description"
-        value={description}
-        onChange={event => handleOnChange("description", event.target.value)}
-      />
-      <div>
-        <Button onClick={handleOnClick}>Contact Us</Button>
-      </div>
+      { !sent && 
+        <div className="formContainer">
+          {sendError && <div>{sendError}</div>}
+          <h5>
+            Let us know how we can help <i>you</i>
+          </h5>
+          <TextInput
+            label="Name"
+            value={name}
+            onChange={event => handleOnChange("name", event.target.value)}
+          />
+          <TextInput
+            label="Email"
+            value={email}
+            onChange={event => handleOnChange("email", event.target.value)}
+          />
+          <TextInput
+            label="Subject"
+            value={subject}
+            onChange={event => handleOnChange("subject", event.target.value)}
+          />
+          <Textarea
+            label="Description"
+            value={description}
+            onChange={event => handleOnChange("description", event.target.value)}
+          />
+          <div>
+            <Button onClick={handleOnClick}>Contact Us</Button>
+          </div>
+        </div>
+      }
+      { sent && !loading && 
+        <div className="successContainer">
+          <div>Message sent! </div>
+        </div>
+      }
+      
+      
     </div>
   )
 }
