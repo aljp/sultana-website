22c938485743bfa738fe64546a139ba65b413583
Adam Jacquier-Parr <aljparr0@gmail.com>
2020-10-08 22:57:31 +1100

    change generic header

---
+++ src/components/generic-header/GenericHeader.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './GenericHeader.scss';
+
+const GenericHeader = () => {
+  return (
+    <div className="generic-header">
+      <div className="generic-header-container">
+        <div className="header-text">
+          <h2>About Us</h2>
+          <h3>A web design &amp; development firm based in Sydney</h3>
+        </div>
+      </div>
+      
+    </div>
+  )
+};
+
+export default GenericHeader;
