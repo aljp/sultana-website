0c1a962195877a517bdb26a168ffa5fec569a92e
Daniel Teale <djteale90@gmail.com>
2020-10-17 13:32:20 +1100

    Fixed form labels (#13)

--- src/components/form-helpers/Textarea.jsx
+++ src/components/form-helpers/Textarea.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Textarea = (props) => {
   const { label, id, value, className, labelClass, inputClass, onChange } = props;
   const classes = `input-field ${className || ''}`
   const inputClasses = `materialize-textarea ${inputClass || ''}`
+  const labelRef = useRef(null)
   
+  const handleFocus = () => {
+    labelRef.current.classList.add('active')
+  }
+
+  const handleBlur = () => {
+    if (value) return;
+    labelRef.current.classList.remove('active')
+  }
+
   return (
     <div className={classes}>
-      <textarea id={id} value={value} className={inputClasses} onChange={onChange} />
-      <label htmlFor={id} className={labelClass}>{label}</label>
+      <textarea 
+        id={id} 
+        value={value}
+        className={inputClasses} 
+        onChange={onChange} 
+        onFocus={handleFocus} 
+        onBlur={handleBlur}
+      />
+      <label htmlFor={id} className={labelClass} ref={labelRef}>{label}</label>
     </div>
   );
 };
