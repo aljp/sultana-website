bc86b7ab04fcab9f3f3733616f6fdf8e4bb4ce3f
Daniel Teale <djteale90@gmail.com>
2020-11-07 16:26:21 +1100

    minor fixes for deploy

--- src/components/home/ServicesCards.jsx
+++ src/components/home/ServicesCards.jsx
@@ -4,7 +4,8 @@ import CircleSvg from "components/CircleSvg"
 import { ReactComponent as Development } from "images/serviceCards/004-programming-1.svg"
 import { ReactComponent as Optimization } from "images/serviceCards/002-computer.svg"
 import { ReactComponent as Seo } from "images/serviceCards/005-analytics.svg"
-import { ReactComponent as Ecommerce } from "images/serviceCards/shop.svg"
+import { ReactComponent as Analytics } from "images/serviceCards/data-analytics.svg"
+import Card from 'components/generic-components/Card'
 
 const ServiceCards = props => {
   const data = useStaticQuery(graphql`
@@ -23,7 +24,7 @@ const ServiceCards = props => {
     development: <Development />,
     optimization: <Optimization />,
     seo: <Seo />,
-    ecommerce: <Ecommerce />,
+    analytics: <Analytics />,
   }
 
   const handleOnClick = e => e.currentTarget.classList.toggle("active")
@@ -35,31 +36,32 @@ const ServiceCards = props => {
       </div>
       <div className="u-flex u-flexJustifyCenter u-flexWrap service-cards">
         {data.homeJson.serviceCards.map(card => (
-          <div
-            key={card.name}
-            className="service-card-wrapper"
-            onClick={handleOnClick}
-            onKeyPress={handleOnClick}
-            role="link"
-            tabIndex={0}
-          >
-            <div className="service-card">
-              <div
-                className="u-flex u-flexJustifyCenter"
-                style={{ marginBottom: "20px" }}
-              >
-                <div
-                  className="service-card-icon-wrapper"
-                  style={{ width: "100px", height: "100px" }}
-                >
-                  <CircleSvg />
-                  {iconMap[card.icon]}
-                </div>
-              </div>
-              <p>{card.content}</p>
-              <div className="card-body">{card.body}</div>
-            </div>
-          </div>
+          <Card title={card.name} content={`${card.content} ${card.body}`} icon={iconMap[card.icon]} />
+          // <div
+          //   key={card.name}
+          //   className="service-card-wrapper"
+          //   onClick={handleOnClick}
+          //   onKeyPress={handleOnClick}
+          //   role="link"
+          //   tabIndex={0}
+          // >
+          //   <div className="service-card">
+          //     <div
+          //       className="u-flex u-flexJustifyCenter"
+          //       style={{ marginBottom: "20px" }}
+          //     >
+          //       <div
+          //         className="service-card-icon-wrapper"
+          //         style={{ width: "100px", height: "100px" }}
+          //       >
+          //         <CircleSvg />
+          //         {i}
+          //       </div>
+          //     </div>
+          //     <p>{card.content}</p>
+          //     <div className="card-body">{card.body}</div>
+          //   </div>
+          // </div>
         ))}
       </div>
     </div>
