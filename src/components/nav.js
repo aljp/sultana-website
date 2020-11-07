a2d17c05353fc7c8f503674b9ba6a9592d5c14f9
Daniel Teale <djteale90@gmail.com>
2020-11-07 20:31:33 +1100

    added sub links to nav (#40)
    * added sub links to nav
    
    * tidy

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,21 @@
-import React, { useRef } from "react"
+import React, { useState, useRef } from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import logo from 'images/logos/SULTANA_white.svg'
 import { ReactComponent as Menu } from 'images/menu.svg';
+import Dropdown from "./layout/Dropdown";
 
 const Nav = ({ siteTitle }) => {
   const menuRef = useRef(null);
   const menuButtonRef = useRef(null);
+
+  const [dropdownActive, setDropdownActive] = useState(false)
+
+  const handleMouseEnter = () => {
+    setDropdownActive(true);
+  }
+
+  const handleOnMouseLeave = () => { setDropdownActive(false); }
 
   const handleOnClick = () => {
     menuRef.current.classList.toggle('open')
@@ -33,7 +42,8 @@ const Nav = ({ siteTitle }) => {
           <Link to="/about">About Us</Link>
         </li>
         <li>
-          <Link to="/services">Services</Link>
+          <Link to="/services" onMouseEnter={handleMouseEnter}>Services <span className="link-chevron">&rsaquo;</span></Link>
+          <Dropdown active={dropdownActive} onMouseLeave={handleOnMouseLeave}/>
         </li>
         <li>
           <Link to="/contact">Contact</Link>
