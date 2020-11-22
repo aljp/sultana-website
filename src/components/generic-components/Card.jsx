3ee4eae16d96f2f43c84d39958869c5df63911bd
Daniel Teale <djteale90@gmail.com>
2020-11-22 18:22:41 +1100

    updated home page add card links (#51)

--- src/components/generic-components/Card.jsx
+++ src/components/generic-components/Card.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
+import { Link } from 'gatsby'
 import CircleSvg from "components/CircleSvg"
 
-const Card = (props) => {
-  const { icon, content, title } = props;
-  
+const CardContent = (props) => {
+  const { icon, content, title, type } = props;
+
   return (
-    <div
-      className="service-card-wrapper"
-    >
       <div className="service-card">
         <div
           className="u-flex u-flexJustifyCenter"
@@ -26,6 +24,37 @@ const Card = (props) => {
           {content}
         </p>
       </div>
+  )
+}
+
+const LinkedCardContent = (props) => {
+  const { icon, content, title, type } = props;
+  const link = type ? `/${type}` : `#`
+
+  return (
+    <Link to={link}>
+      <CardContent {...props}/>
+    </Link>
+  )
+}
+
+const Card = (props) => {
+  const { icon, content, title, type } = props;
+
+  const link = type ? `/${type}` : undefined
+  let contentElement;
+
+  if (link) {
+    contentElement = <LinkedCardContent {...props}/>
+  } else {
+    contentElement = <CardContent {...props}/>
+  }
+  
+  return (
+    <div
+      className="service-card-wrapper "
+    >
+    {contentElement}
     </div>
   )
 };
